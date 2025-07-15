1b034899947760fdd2b69965f8d1748417b13c8c
DiegoMao201 <diegomao.201@gmail.com>
2025-07-15 15:11:34 -0500

    Update 🏠 Resumen_Mensual.py

--- 🏠 Resumen_Mensual.py
+++ 🏠 Resumen_Mensual.py
@@ -73,12 +73,6 @@ APP_CONFIG['categorias_clave_venta'] = [normalizar_texto(cat) for cat in APP_CON
 
 @st.cache_data(ttl=1800)
 def cargar_y_limpiar_datos(ruta_archivo, nombres_columnas):
-    """
-    Carga un archivo CSV desde Dropbox, lo decodifica, lo lee en un DataFrame de pandas
-    y realiza una limpieza inicial de datos.
-    Maneja el error "No columns to parse from file" verificando si el DataFrame
-    está vacío inmediatamente después de la lectura.
-    """
     try:
         with dropbox.Dropbox(app_key=st.secrets.dropbox.app_key, app_secret=st.secrets.dropbox.app_secret, oauth2_refresh_token=st.secrets.dropbox.refresh_token) as dbx:
             _, res = dbx.files_download(path=ruta_archivo)
